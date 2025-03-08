aa4485d79f01dc9259ca3429dffb0fe84e092fc9
Gozdealc <gozdeealici@gmail.com>
2025-03-08 16:14:58 +0300

    giriş yazısı Gözde tarafından değiştirildi.

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-        Archis Academy'e hoş geldiniz!!
+        Archis Academy'e hoş geldiniz!! Gözde Alıcı 
         </p>
       </div>
       <p className="read-the-docs">
